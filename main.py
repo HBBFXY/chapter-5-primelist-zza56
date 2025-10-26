385fcefdc59d9d12e185cc04bb9ecaf194e41adb
zza56 <1514326359@qq.com>
2025-10-26 14:13:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-def PrimeList(N):
-    primes = []
-    for num in range(2, N):
-        is_prime = True
-        # 检查num是否能被小于等于其平方根的数整除
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(str(num))
-    # 用空格连接所有质数字符串
-    return ' '.join(primes)
+daily_improvement.py
+ ability = 1
+day_in_cycle = 0
+for _ in range(365):
+    day_in_cycle += 1
+    if day_in_cycle > 3 and day_in_cycle <= 7:
+        ability *= 1.01
+    if day_in_cycle == 7:
+        day_in_cycle = 0
+print(ability)
